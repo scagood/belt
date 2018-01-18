6c74c5401d3cc3d2806e5676776565d76f79a4aa
Sca <scagood@me.com>
2018-01-18 10:07:04 +0000

    Update dev.js

--- src/dev.js
+++ src/dev.js
@@ -1,57 +1,99 @@
-var comp = require('./compare');
-var math = require('./maths');
+const comp = require('./compare');
+const math = require('./maths');
 
-var trigonometry = function (input) {
-    var typeDetect = '';
-    var output = {
-        a: input.a,
-        b: input.b,
-        c: input.c,
-        A: input.A,
-        B: input.B,
-        C: input.C
-    };
+const cosineRuleLength = math.cosineRuleLength;
+const cosineRuleAngle = math.cosineRuleAngle;
+const sineRuleLength = math.sineRuleLength;
+const sineRuleAngle = math.sineRuleAngle;
+
+const trigonometry = function (input) {
+    let typeDetect = '';
+    let a = input.a;
+    let b = input.b;
+    let c = input.c;
+    let A = input.A;
+    let B = input.B;
+    let C = input.C;
 
     // Convert inputs into a binary string.
-    typeDetect += comp.isNum(input.a) ? '1' : '0';
-    typeDetect += comp.isNum(input.b) ? '1' : '0';
-    typeDetect += comp.isNum(input.c) ? '1' : '0';
-    typeDetect += comp.isNum(input.A) ? '1' : '0';
-    typeDetect += comp.isNum(input.B) ? '1' : '0';
-    typeDetect += comp.isNum(input.C) ? '1' : '0';
+    typeDetect += comp.isNum(a) ? '1' : '0';
+    typeDetect += comp.isNum(b) ? '1' : '0';
+    typeDetect += comp.isNum(c) ? '1' : '0';
+    typeDetect += comp.isNum(A) ? '1' : '0';
+    typeDetect += comp.isNum(B) ? '1' : '0';
+    typeDetect += comp.isNum(C) ? '1' : '0';
 
     switch (typeDetect) {
         case '111000':
-            // cosine 3 times
-            output.A = math.cosineRuleAngle(input.a, input.b, input.c);
-            output.B = math.cosineRuleAngle(input.b, input.a, input.c);
-            output.C = math.cosineRuleAngle(input.c, input.a, input.b);
+            // a, b, c
+            A = cosineRuleAngle(a, b, c);
+            B = cosineRuleAngle(b, a, c);
+            C = cosineRuleAngle(c, a, b);
             break;
+        
         case '110100':
+            // a, b, A
+            B = sineRuleAngle(a, A, b);
+            C = Math.PI / 2 - A - B;
+            c = cosineRuleLength(a, b, C);
             break;
         case '110010':
+            // a, b, B
+            A = sineRuleAngle(b, B, a);
+            C = Math.PI / 2 - A - B;
+            c = cosineRuleLength(a, b, C);
             break;
         case '110001':
+            // a, b, C
+            c = cosineRuleLength(a, b, C);
+            A = cosineRuleAngle(a, b, c);
+            B = sineRuleAngle(a, A, b);
             break;
+            
+        case '101100':
+            // a, c, A
+            C = sineRuleAngle(a, A, c);
+            B = Math.PI / 2 - A - C;
+            b = sineRuleLength(a, A, B);
+            break;
+        case '101010':
+            // a, c, B
+            b = cosineRuleLength(a, c, B);
+            A = cosineRuleAngle(a, b, c);
+            B = cosineRuleAngle(b, c, a);
+            break;
+        case '101001':
+            // a, c, C
+            A = sineRuleAngle(c, C, a);
+            B = Math.PI / 2 - A - C;
+            b = sineRuleLength(a, A, B);
+            break;
+            /*
+        case '011100':
+            // b, c, A
+            C = sineRuleAngle(a, A, c);
+            B = Math.PI / 2 - A - C;
+            b = sineRuleLength(a, A, B);
+            break;
+        case '011010':
+            // b, c, B
+            b = cosineRuleLength(a, c, B);
+            A = cosineRuleAngle(a, b, c);
+            B = cosineRuleAngle(b, c, a);
+            break;
+        case '011001':
+            // b, c, C
+            A = sineRuleAngle(c, C, a);
+            B = Math.PI / 2 - A - C;
+            b = sineRuleLength(a, A, B);
+            break;
+            */
         default:
             output.error = 'Not enough information';
             break;
 
     }
 };
-
-/* Random string gen from php
-function randomString($length = 8) {
-    $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-    $string = array(); //remember to declare $string as an array
-    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-    for ($i = 0; $i < $length; $i++) {
-        $n = rand(0, $alphaLength);
-        $string[] = $alphabet[$n];
-    }
-    return implode($string); //turn the array into a string
-}
-*/
 
 module.exports = {
     trigonometry: trigonometry
